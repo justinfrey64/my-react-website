2e942f29660234281106c1064a8eb09c43c31b76
Justin Frey <justinfrey64@gmail.com>
2019-06-24 08:39:49 -0400

    Update Home.js

--- src/home/Home.js
+++ src/home/Home.js
@@ -7,15 +7,15 @@ class Home extends Component {
       <div>
         <h1 class="intro-title mb-4">I am Justin Frey</h1>
         <Typed 
-                strings={[
-                    'Frontend Developer',
-                    'Web Designer',
-                    'Backend Developer',
-                    'IT Specialist']}
-                    typeSpeed={50}
-                    backSpeed={60} 
-                    loop >
-                </Typed>
+          strings={[
+              'Frontend Developer',
+              'Web Designer',
+              'Backend Developer',
+              'IT Specialist']}
+          typeSpeed={50}
+          backSpeed={60} 
+          loop >
+        </Typed>
       </div>
     );
   }
