65023600284e08c176344ad1cba99cf5afb2a076
Justin Frey <justinfrey64@gmail.com>
2019-10-23 15:52:13 -0400

    Updates

---
+++ precache-manifest.18b16ee1d2bfd7213a279b74e83716bf.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "805db5cddb84ca48ba0c0d441d77c94b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b99822eed3ef3134cbc2",
+    "url": "/static/css/2.33627280.chunk.css"
+  },
+  {
+    "revision": "a2cbfb1da2abf6bdb35d",
+    "url": "/static/css/main.f3e78bb1.chunk.css"
+  },
+  {
+    "revision": "b99822eed3ef3134cbc2",
+    "url": "/static/js/2.4cc98d2a.chunk.js"
+  },
+  {
+    "revision": "a2cbfb1da2abf6bdb35d",
+    "url": "/static/js/main.65343ce7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "42342a7bbc4f46f88527ece461bca024",
+    "url": "/static/media/inside-computer-square.42342a7b.jpg"
+  },
+  {
+    "revision": "4c0c46300d0aa319b20abfe1523a7b11",
+    "url": "/static/media/killington.4c0c4630.jpg"
+  },
+  {
+    "revision": "f6a9dd5cef00844f2e004674ad538744",
+    "url": "/static/media/laptop-stickers.f6a9dd5c.jpg"
+  },
+  {
+    "revision": "78356b75ae8cec9e1f480b0189cc66a7",
+    "url": "/static/media/old-headshot.78356b75.png"
+  },
+  {
+    "revision": "ac7e6909fd9c8c55d2137adcc96812d7",
+    "url": "/static/media/resume-summer-2019.ac7e6909.pdf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/my-react-website/precache-manifest.145d54c9be59b2ee10205e7426dc9d8b.js"
+  "/precache-manifest.18b16ee1d2bfd7213a279b74e83716bf.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/my-react-website/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
